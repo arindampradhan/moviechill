f323c19046059986b09d211fc75acd8eb05befe2
arindampradhan <arindampradhan10@gmail.com>
2018-12-02 13:14:31 +0530

    app initialized

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { observer } from "mobx-react";
+import { observable } from "mobx";
+
 import './App.css';
 
+
+@observer
 class App extends Component {
+  @observable boxVisible = true;
+  toggleBox = () => {
+    this.boxVisible = !this.boxVisible;
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>MobX Demo</h2>
+        <button onClick={this.toggleBox}>Toggle Box</button>
+        {this.boxVisible && <div style={styles.box} />}
       </div>
     );
   }
 }
+
+const styles = {
+  box: {
+    margin: 10,
+    height: 200,
+    width: 200,
+    backgroundColor: "red"
+  }
+};
+
 
 export default App;
